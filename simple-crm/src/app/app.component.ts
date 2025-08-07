1da1d692364415140c273c065754baa91748cc27
Kay <118884667+BadRix90@users.noreply.github.com>
2025-08-07 09:34:02 +0200

    create project

--- simple-crm/src/app/app.component.ts
+++ simple-crm/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule, 
+    MatButtonModule
+  ],
+   host: { ngSkipHydration: '' },
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
